145da0d75e2606e03185b046da3023676756d385
Nicolas Jaramillo <151190011+The-Nick-J@users.noreply.github.com>
2024-01-24 14:17:41 +0000

    todolist

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,42 @@
 import React, { useState } from "react";
 import "./App.css";
+import { NewTodoForm } from "./NewTodoForm";
+import { TodoList } from "./TodoList";
+
+
 const App = () => {
-    const [newItem, setNewItem] = useState("")
+  const [todos, setTodos] = useState([]);
+
+  function addTodo(title) {
+    setTodos((currentTodos) => {
+      return [
+        ...currentTodos,
+        { id: crypto.randomUUID(), title, completed: false },
+      ];
+    });
+  }
+
+  function toggleTodo(id, completed) {
+    setTodos((currentTodos) => {
+      return currentTodos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, completed };
+        }
+        return todo;
+      });
+    });
+  }
+
+  function deleteTodo(id) {
+    setTodos((currentTodos) => {
+      return currentTodos.filter((todo) => todo.id !== id);
+    });
+  }
   return (
     <>
-      <form className="new-item-form">
-        <div className="form-row">
-          <label htmlFor="item">New Item</label>
-          <input type="text" id="item" />
-        </div>
-        <button className="btn">Add</button>
-      </form>
+      <NewTodoForm addTodo={addTodo} />
       <h1 className="header">To Do List</h1>
-      <ul className="list">
-        <li>
-        <label>
-            <input type="checkbox" />
-            Primer Item
-        </label>
-        <button className="btn btn-danger">X</button>
-        </li>
-      </ul>
+      <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
     </>
   );
 };
