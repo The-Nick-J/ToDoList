f3f1621f5705adb37a916dd678c3bdfed9b9eba1
Nicolas Jaramillo <151190011+The-Nick-J@users.noreply.github.com>
2024-01-24 12:59:34 +0000

    empezando

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
-
+import React, { useState } from "react";
+import "./App.css";
 const App = () => {
-    return (
-        <h1>REACT APP</h1>
-    )
-}
+    const [newItem, setNewItem] = useState("")
+  return (
+    <>
+      <form className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="item">New Item</label>
+          <input type="text" id="item" />
+        </div>
+        <button className="btn">Add</button>
+      </form>
+      <h1 className="header">To Do List</h1>
+      <ul className="list">
+        <li>
+        <label>
+            <input type="checkbox" />
+            Primer Item
+        </label>
+        <button className="btn btn-danger">X</button>
+        </li>
+      </ul>
+    </>
+  );
+};
 
 export default App;
